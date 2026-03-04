4d57a6fa77773276a41c60c6bd1c4bc0387d5558
vivaswanghosh <vivaswan.ghosh@gmail.com>
2026-03-05 01:46:46 +0530

    Demo user

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     port: 3000,
     strictPort: false,
-    open: true,
+    open: false,
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
